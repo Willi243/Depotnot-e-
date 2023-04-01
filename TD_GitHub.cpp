f755bacfe187dfb587967e78989a35f2c5620a7b
Willi243 <128788745+Willi243@users.noreply.github.com>
2023-04-02 01:58:55 +0200

    Update TD_GitHub.cpp

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -11,13 +11,69 @@
 
 //Fonctions réalisées par l'étudiant 2 :
 
+bool estPaire(int nb) {
 
+	if (nb%2==0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+	//return (nb % 2 == 0);
+}
 
+int sommeNombre(int nb) {
 
+	for (int i = 1;  i < nb; i++)
+	{
+		nb = nb + i;
+	}
+	return nb;
 
+}
 
+float salaireNet(int brut) {
 
+	return brut * (1 - 23 / 100);
+}
+
+int plusPetit(int a,int b,int c) {
+
+	if (a<b && a<c)
+	{
+		return a;
+	}
+
+	else if (b<a && b<c)
+	{
+		return b;
+	}
+	else 
+	{
+		return c;
+	}
+
+}
 
 
 
 //Fonctions réalisées par l'étudiant 3 :
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+
+}
